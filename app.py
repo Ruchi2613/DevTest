384dfbecea154e92b6655e0e1d768d421a1b765f
Ruchi2613 <ruchipandey2606@gmail.com>
2024-12-17 00:55:46 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+    # return "Hello, Flask!"
+
+@app.route('/products')
+def products():
+    return "this is product page hubbu!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
